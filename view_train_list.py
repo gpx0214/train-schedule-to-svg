ebf4be2a882fed2df70f771868dbe4767cb9f370
gpx0214 <gpx0214@gmail.com>
2018-09-24 18:22:30 +0800

    增加输出svg函数

--- view_train_list.py
+++ view_train_list.py
@@ -40,6 +40,72 @@ def cmpbyTime(a1, a2):
     return 0
 
 
+def cmpbyTrain(a1, a2):
+    if (len(a1) < 5):
+        return 0
+    if (len(a2) < 5):
+        return 0
+    train1 = a1[0]
+    train2 = a2[0]
+    if train1 > train2:
+        return 1
+    if train1 < train2:
+        return -1
+    d1 = int(a1[2])
+    d2 = int(a2[2])
+    if d1 > d2:
+        return 1
+    if d1 < d2:
+        return -1
+    t1 = getmin(a1[3])
+    t2 = getmin(a2[3])
+    if t1 > t2:
+        return 1
+    if t1 < t2:
+        return -1
+    if int(a1[4]) > int(a2[4]):
+        return 1
+    if int(a1[4]) < int(a2[4]):
+        return -1
+    return 0
+
+
+def cmpbyTrain0(a1, a2):
+    if (len(a1) < 5):
+        return 0
+    if (len(a2) < 5):
+        return 0
+    train1 = a1[0]
+    train2 = a2[0]
+    if train1 > train2:
+        return 1
+    if train1 < train2:
+        return -1
+    n1 = int(a1[2])
+    n2 = int(a2[2])
+    if n1 > n2:
+        return 1
+    if n1 < n2:
+        return -1
+    d1 = int(a1[3])
+    d2 = int(a2[3])
+    if d1 > d2:
+        return 1
+    if d1 < d2:
+        return -1
+    t1 = getmin(a1[4])
+    t2 = getmin(a2[4])
+    if t1 > t2:
+        return 1
+    if t1 < t2:
+        return -1
+    if int(a1[4]) > int(a2[5]):
+        return 1
+    if int(a1[4]) < int(a2[5]):
+        return -1
+    return 0
+
+
 def getmin(str):
     try:
         a, b = str.split(':')[0:2]
@@ -196,7 +262,7 @@ def savecsv(t, station):
                 s = schToCsv(schdata)
                 num += len(s)
                 for row in s:
-                    #time_list.append(row)
+                    # time_list.append(row)
                     if len(row) >= 6:
                         minute = getmin(row[4])
                         #stat[minute] += 1
@@ -247,7 +313,8 @@ def schToCsv(s):
             '''buffer += (s[0]['station_train_code'].encode('utf-8') + ',' + s[i]['station_name'].encode('utf-8') + ',' + s[i]['station_no'].encode('utf-8')
                        + ',' + str(day) + ',' + s[i]['arrive_time'].encode('utf-8') + ',' + '0'+'\n')'''
             ret.append([
-                s[0]['station_train_code'].encode('utf-8'), s[i]['station_name'].encode('utf-8'), s[i]['station_no'].encode('utf-8'),
+                s[0]['station_train_code'].encode(
+                    'utf-8'), s[i]['station_name'].encode('utf-8'), s[i]['station_no'].encode('utf-8'),
                 str(day), s[i]['arrive_time'].encode('utf-8'), '0'
             ])
 
@@ -261,31 +328,35 @@ def schToCsv(s):
             '''buffer += (s[0]['station_train_code'].encode('utf-8') + ',' + s[i]['station_name'].encode('utf-8') + ',' + s[i]['station_no'].encode('utf-8')
                        + ',' + str(day) + ',' + s[i]['start_time'].encode('utf-8') + ',' + '1'+'\n')'''
             ret.append([
-                s[0]['station_train_code'].encode('utf-8'), s[i]['station_name'].encode('utf-8'), s[i]['station_no'].encode('utf-8'),
+                s[0]['station_train_code'].encode(
+                    'utf-8'), s[i]['station_name'].encode('utf-8'), s[i]['station_no'].encode('utf-8'),
                 str(day), s[i]['start_time'].encode('utf-8'), '1'
             ])
     # return buffer
     return ret
 
+
 def readcsv(fn):
     with open(fn, 'r') as f:  # py2
-        if f.read(3)!='\xef\xbb\xbf':
-            f.seek(0,0)
+        if f.read(3) != '\xef\xbb\xbf':
+            f.seek(0, 0)
         data = f.read()
     c = data.split('\n')
     for i in range(len(c)):
-        c[i] = c[i].split(' ')
+        c[i] = c[i].split(',')
     return c
+
 
 def openMilage(fn):
     with open(fn, 'r') as f:  # py2
-        if f.read(3)!='\xef\xbb\xbf':
-            f.seek(0,0)
+        if f.read(3) != '\xef\xbb\xbf':
+            f.seek(0, 0)
         data = f.read()
     m = data.split('\n')
     for i in range(len(m)):
         m[i] = m[i].split(' ')
     return m
+
 
 def getkm(s, m):
     for i in range(len(m)):
@@ -293,25 +364,26 @@ def getkm(s, m):
             return m[i][1]
     return -1
 
-#(1440-x0)/x1=(y-y0)(y1-y) = k
-#(y-y0)=(y1-y)*(1440-x0)/x1
-#(y-y0)=(y1-y)*k  
-#y=y0+y1*k-y*k
-#(1+k)y=y0+y1*k
-#y=(y0+y1*k)/(1+k)
-#k:
-#y=y0/(1+k)+y1*(1+k-1)/(1+k)
-#y=y0/(1+k)+y1-y1/(1+k)
-#y=(y0-y1)/(1+k)+y1
-#(lasty-y)/(1+(1440-lastx)/x)+y
-#(lasty-y)*x/((1440+x-lastx))+y
-#1/k:
-#y=y0(1+k-k)/(1+k)+y1*k/(1+k)
-#y=y0-y0*k/(1+k)+y1*k/(1+k)
-#y=y0+(y1-y0)*k/(1+k)
-#y=y0+(y1-y0)*(1/(1/k))/((1/k)/(1/k)+(1/(1/k)))
-#y=y0+(y1-y0)*1/((1/k)+1)
-#lasty+(lasty-y)/(1+x/(1440-lastx))
+# (1440-x0)/x1=(y-y0)(y1-y) = k
+# (y-y0)=(y1-y)*(1440-x0)/x1
+# (y-y0)=(y1-y)*k
+# y=y0+y1*k-y*k
+# (1+k)y=y0+y1*k
+# y=(y0+y1*k)/(1+k)
+# k:
+# y=y0/(1+k)+y1*(1+k-1)/(1+k)
+# y=y0/(1+k)+y1-y1/(1+k)
+# y=(y0-y1)/(1+k)+y1
+# (lasty-y)/(1+(1440-lastx)/x)+y
+# (lasty-y)*x/((1440+x-lastx))+y
+# 1/k:
+# y=y0(1+k-k)/(1+k)+y1*k/(1+k)
+# y=y0-y0*k/(1+k)+y1*k/(1+k)
+# y=y0+(y1-y0)*k/(1+k)
+# y=y0+(y1-y0)*(1/(1/k))/((1/k)/(1/k)+(1/(1/k)))
+# y=y0+(y1-y0)*1/((1/k)+1)
+# lasty+(lasty-y)/(1+x/(1440-lastx))
+
 
 def schToPolyline(s, m):
     if (len(s)) <= 0:
@@ -320,34 +392,108 @@ def schToPolyline(s, m):
     day = 0
     lastx = 0
     lasty = 0
-    buffer += '<polyline name="%s" class="G" style="fill:none;stroke:blue;stroke-width:1;opacity:0.8" points="' % (s[0]['station_train_code'].encode('utf-8'))
+    buffer += '<polyline name="%s" class="G" style="fill:none;stroke:blue;stroke-width:1;opacity:0.8" points="' % (
+        s[0]['station_train_code'].encode('utf-8'))
     for i in range(0, len(s)):
         x = getmin(s[i]['arrive_time'].encode('utf-8'))
         y = getkm(s[i]['station_name'].encode('utf-8'), m)
-        if getmin(s[i]['arrive_time'].encode('utf-8')) > -1 and i > 0:
+        if x > -1 and i > 0:
             if x < lastx:
                 day += 1
                 #1440, (lasty-y)*x/((1440+x-lastx))+y
-                buffer += '%d,%d "/>\n<polyline name="%s+%d" class="G" style="fill:none;stroke:blue;stroke-width:1;opacity:0.8" points="%d,%d' \
-                    % (1440, (int(lasty)-int(y))*int(x)/((1440+int(x)-int(lastx)))+int(y), s[0]['station_train_code'].encode('utf-8'), day,\
-                    1440, (int(lasty)-int(y))*int(x)/((1440+int(x)-int(lastx)))+int(y) )
+                buffer += '%d,%d "/>\n<polyline name="%s+%d" class="G" style="fill:none;stroke:blue;stroke-width:1;opacity:0.8" points="%d,%d ' \
+                    % (1440, (int(lasty)-int(y))*int(x)/((1440+int(x)-int(lastx)))+int(y), s[0]['station_train_code'].encode('utf-8'), day,
+                       0, (int(lasty)-int(y))*int(x)/((1440+int(x)-int(lastx)))+int(y))
             lastx = x
             lasty = y
         buffer += '%s,%s ' % (x, y)
-        
+
         x = getmin(s[i]['start_time'].encode('utf-8'))
         y = getkm(s[i]['station_name'].encode('utf-8'), m)
-        if getmin(s[i]['start_time'].encode('utf-8')) > -1 and i < len(s)-1:
+        if x > -1 and i < len(s)-1:
             if x < lastx:
                 day += 1
-                buffer += '%d,%d "/>\n<polyline name="%s+%d" class="G" style="fill:none;stroke:blue;stroke-width:1;opacity:0.8" points="%d,%d' \
-                    % (1440, (int(lasty)-int(y))*int(x)/((1440+int(x)-int(lastx)))+int(y), s[0]['station_train_code'].encode('utf-8'), day,\
-                    1440, (int(lasty)-int(y))*int(x)/((1440+int(x)-int(lastx)))+int(y) )
+                buffer += '%d,%d "/>\n<polyline name="%s+%d" class="G" style="fill:none;stroke:blue;stroke-width:1;opacity:0.8" points="%d,%d ' \
+                    % (1440, (int(lasty)-int(y))*int(x)/((1440+int(x)-int(lastx)))+int(y), s[0]['station_train_code'].encode('utf-8'), day,
+                       0, (int(lasty)-int(y))*int(x)/((1440+int(x)-int(lastx)))+int(y))
             lastx = x
             lasty = y
         buffer += '%s,%s ' % (x, y)
     buffer += '"/>\n'
     return buffer
+
+
+def csvToPolyline(c, m):
+    # ['K868,\xe6\xb3\x8a\xe5\xa4\xb4,05,1,00:00,0']
+    if (len(c)) <= 0:
+        return ''
+    buffer = ''
+    day = 0
+    lastx = 0
+    lasty = 0
+    buffer += '<polyline name="%s" class="G" style="fill:none;stroke:blue;stroke-width:1;opacity:0.8" points="' % (
+        c[0][0])
+    for i in range(0, len(c)):
+        x = getmin(c[i][4])
+        y = getkm(c[i][1], m)
+        if x > -1 and i > 0:
+            if x < lastx:
+                day += 1
+                #1440, (lasty-y)*x/((1440+x-lastx))+y
+                buffer += '%d,%d "/>\n<polyline name="%s+%d" class="G" style="fill:none;stroke:blue;stroke-width:1;opacity:0.8" points="%d,%d ' \
+                    % (1440, (int(lasty)-int(y))*int(x)/((1440+int(x)-int(lastx)))+int(y), c[0][0], day,
+                       0, (int(lasty)-int(y))*int(x)/((1440+int(x)-int(lastx)))+int(y))
+            lastx = x
+            lasty = y
+        buffer += '%s,%s ' % (x, y)
+    buffer += '"/>\n'
+    return buffer
+
+
+def csvToSvg(m, c):
+    maxlen = 70000
+    arr = [[] for i in range(maxlen)]
+
+    for i in range(len(c)):
+        if len(c[i]) < 2:
+            continue
+        if getkm(c[i][1], m) > -1:
+            # print(hash_no(c[i][0]))
+            arr[hash_no(c[i][0])].append(c[i])
+
+    buffer = ''
+    buffer += '<?xml version="1.0" standalone="no"?>\n'
+    buffer += '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" \n'
+    buffer += '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n'
+    buffer += '<svg width="%s" height="%s" version="1.1" \n' % (1440, 2450)
+    buffer += 'xmlns="http://www.w3.org/2000/svg">\n'
+
+    for i in range(len(m)):
+        buffer += ('<text x="0" y="%d">%s %s</text>\n' %
+                   (int(m[i][1]) if int(m[i][1]) > 16 else (int(m[i][1]) + 16), m[i][0], m[i][1]))
+        buffer += ('<line x1="0" y1="%s" x2="1440" y2="%s" style="stroke:rgb(220,220,220);stroke-width:1"/>\n' %
+                   (m[i][1], m[i][1]))
+
+    for i in range(24):
+        buffer += ('<line x1="%d" y1="0" x2="%d" y2="3000" style="stroke:rgb(128,128,128);stroke-width:1"/>\n' % (i*60, i*60))
+        buffer += ('<line x1="%d" y1="0" x2="%d" y2="3000" style="stroke:rgb(220,220,220);stroke-width:1"/>\n' %
+                   (i*60+30, i*60+30))
+
+    num = 0
+    for i in range(maxlen):
+        flag = 0
+        if len(arr[i]) > 2:
+            flag = 1
+        if len(arr[i]) == 2:
+            if arr[i][0][1] != arr[i][1][1]:
+                flag = 1
+        if flag:
+            num += 1
+            arr[i] = sorted(arr[i], cmpbyTrain0)
+            buffer += csvToPolyline(arr[i], m)
+
+    buffer += ('</svg>')
+    return buffer, num
 
 
 def train_list_type_str(t):
@@ -434,7 +580,7 @@ if __name__ == '__main__':
         print(train_list_type_str(t))
         station = getStation(fn1)
         checkAllSch12306(t, station)
-        savecsv(t,station)
+        savecsv(t, station)
         if platform.system() == "Windows":
             os.system('pause')
     except Exception, e:
@@ -448,6 +594,15 @@ from view_train_list import *
 
 t = openTrainList('train_list.js')
 station = getStation('station_name.js')
-savecsv(t,station)
+#savecsv(t,station)
 m = openMilage('test/京广高速线里程.txt')
+c = readcsv('delay/sort2018-09-30.csv')
+buffer,_ = csvToSvg(m, c)
+
+fn = 'delay/0930京广.svg'
+with open(fn, "wb") as f:  # use wb on win, or get more \r \r\n
+    if f.tell() == 0:
+        f.write('\xef\xbb\xbf')
+    f.write(buffer)
+
 '''
