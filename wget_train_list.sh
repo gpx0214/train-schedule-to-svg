db8655710f095d178c5181693e511f1dd8625b70
gpx0214 <gpx0214@gmail.com>
2018-12-01 12:18:41 +0800

    比较两次文件大小

--- wget_train_list.sh
+++ wget_train_list.sh
@@ -3,10 +3,13 @@
 path="/var/ftp/"
 
 t0='0'
+s0='0'
 if [ -f "${path}js/train_list.js" ];then
 t0=`stat -c %Y ${path}js/train_list.js`
+s0=`stat -c %s ${path}js/train_list.js`
 else
 t0='0'
+s0='0'
 fi
 #echo ${t0}
 
@@ -23,20 +26,28 @@ fi
 #/usr/bin/wget --no-check-certificate https://kyfw.12306.cn/otn/resources/js/query/train_list.js -O ${path}js/train_list.js && ${path}js/view_train_list.py ${path}js/train_list.js
 
 t1='0'
+s1='0'
 if [ -f "${path}js/train_list.js" ];then
 t1=`stat -c %Y ${path}js/train_list.js`
+s1=`stat -c %s ${path}js/train_list.js`
 else
 t1='0'
+s1='0'
 fi
 #echo ${t1}
 
 #echo $((${t1}-${t0}))
 if ((${t1} > ${t0}));then
 echo t1 newer
+if ((${s1} != ${s0}));then
 ${path}view_train_list.py ${path}js/train_list.js
 yymmdd=`date +"%y%m%d" -d "$(stat -c %y ${path}js/train_list.js)"`
 cp -p ${path}js/train_list.js ${path}js/train_list_${yymmdd}.js
 gzip -c9 ${path}js/train_list.js > ${path}js/train_list.js.gz
 else
+echo t1 newer but same
+fi
+else
 echo t0 newer or same
 fi
+
