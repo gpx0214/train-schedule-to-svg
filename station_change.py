6f5a1b52772539106f171ef57187226d2b7c987d
gpx0214 <gpx0214@gmail.com>
2019-12-30 00:42:08 +0800

    station_name.js站名变动历史 减少输出内容大小

--- station_change.py
+++ station_change.py
@@ -17,20 +17,28 @@ def unhash_tele(n):
 buffer = ""
 lastmap = ["" for i in range(26*26*26)]
 map = ["" for i in range(26*26*26)]
-for fn in glob.glob(r'js\station_name_*.js'):
+fns = glob.glob(r'js\station_name_*.js')
+for fi in range(len(fns)):
+    fn = fns[fi]
     t1 = os.path.getmtime(fn)
     #print('%d %s'%(t1,fn))
     station = getStation(fn)
     fdate = re.sub(r'js\\station_name_(\d+).js', r'\1', fn)
     for row in station:
         map[hash_tele(row[2])] = row[1]
+    if fi == 0:
+        for i in range(26*26*26):
+            if map[i]:
+                buffer += ('%3s:%s\n' % (unhash_tele(i), map[i]))
+        lastmap = copy.deepcopy(map)
+        continue
     for i in range(26*26*26):
         if map[i] and not lastmap[i]:
-            buffer += ('%s ++++ %3s:%s\n' % (fdate, unhash_tele(i), map[i]))
+            buffer += ('%s + %3s:%s\n' % (fdate, unhash_tele(i), map[i]))
         if not map[i] and lastmap[i]:
-            buffer += ('%s ---- %3s:%s\n' % (fdate, unhash_tele(i), lastmap[i]))
+            buffer += ('%s - %3s:%s\n' % (fdate, unhash_tele(i), lastmap[i]))
         if map[i] and lastmap[i] and map[i] != lastmap[i]:
-            buffer += ('%s      %3s:%s->%s\n' %
+            buffer += ('%s   %3s:%s->%s\n' %
                        (fdate, unhash_tele(i), lastmap[i], map[i]))
     lastmap = copy.deepcopy(map)
 
