a1b59bb85692a0cc4a1662929b11c5cb1569c6a2
gpx0214 <gpx0214@gmail.com>
2020-04-13 00:26:54 +0800

    修复station_change减少车站

--- station_change.py
+++ station_change.py
@@ -6,31 +6,31 @@ import copy
 
 buffer = ""
 lastmap = ["" for i in range(26*26*26)]
-map = ["" for i in range(26*26*26)]
 fns = sorted(glob.glob(r'js/station_name_*.js'))
 for fi in range(len(fns)):
+    curmap = ["" for i in range(26*26*26)]
     fn = fns[fi]
     t1 = os.path.getmtime(fn)
     #print('%d %s'%(t1,fn))
     station = getStation(fn)
     fdate = re.sub(r'js[/\\]station_name_(\d+).js', r'\1', fn)
     for row in station:
-        map[hash_tele(row[2])] = row[1]
+        curmap[hash_tele(row[2])] = row[1]
     if fi == 0:
         for i in range(26*26*26):
-            if map[i]:
-                buffer += ('%3s:%s\n' % (unhash_tele(i), map[i]))
-        lastmap = copy.deepcopy(map)
+            if curmap[i]:
+                buffer += ('%3s:%s\n' % (unhash_tele(i), curmap[i]))
+        lastmap = copy.deepcopy(curmap)
         continue
     for i in range(26*26*26):
-        if map[i] and not lastmap[i]:
-            buffer += ('%s + %3s:%s\n' % (fdate, unhash_tele(i), map[i]))
-        if not map[i] and lastmap[i]:
+        if curmap[i] and not lastmap[i]:
+            buffer += ('%s + %3s:%s\n' % (fdate, unhash_tele(i), curmap[i]))
+        if (not curmap[i]) and lastmap[i]:
             buffer += ('%s - %3s:%s\n' % (fdate, unhash_tele(i), lastmap[i]))
-        if map[i] and lastmap[i] and map[i] != lastmap[i]:
+        if curmap[i] and lastmap[i] and curmap[i] != lastmap[i]:
             buffer += ('%s   %3s:%s->%s\n' %
-                       (fdate, unhash_tele(i), lastmap[i], map[i]))
-    lastmap = copy.deepcopy(map)
+                       (fdate, unhash_tele(i), lastmap[i], curmap[i]))
+    lastmap = copy.deepcopy(curmap)
 
 with open('station_change.txt', 'wb') as f:
     if f.tell() == 0:
