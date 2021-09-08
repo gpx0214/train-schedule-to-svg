70ea1be2608056d9273480ec613a4825b47e97a0
gpx0214 <gpx0214@gmail.com>
2021-09-09 00:44:06 +0800

    使用7za压缩gzipp

--- get_train_csv.sh
+++ get_train_csv.sh
@@ -3,8 +3,15 @@
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 
 ${path}view_train_list.py
-gzip -c9 ${path}js/train.csv > ${path}js/train.csv.gz
-gzip -c9 ${path}js/time.csv > ${path}js/time.csv.gz
-gzip -c9 ${path}js/time.min.csv > ${path}js/time.min.csv.gz
+#gzip -c9 ${path}js/train.csv > ${path}js/train.csv.gz
+#gzip -c9 ${path}js/time.csv > ${path}js/time.csv.gz
+#gzip -c9 ${path}js/time.min.csv > ${path}js/time.min.csv.gz
+
+rm -f ${path}js/train.csv.gz
+7za a -tgzip -mx9 ${path}js/train.csv.gz ${path}js/train.csv >/dev/null
+rm -f ${path}js/time.csv.gz
+7za a -tgzip -mx9 ${path}js/time.csv.gz ${path}js/time.csv >/dev/null
+rm -f ${path}js/time.min.csv.gz
+7za a -tgzip -mx9 ${path}js/time.min.csv.gz ${path}js/time.min.csv >/dev/null
 
 ${path}getemu.sh
